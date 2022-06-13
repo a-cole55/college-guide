35479df09d8de21ed51ee3255567c077dcb151c3
a-cole55 <arniqua_c@yahoo.com>
2022-06-13 16:21:30 -0500

    added individual components for seperate pages; added router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ChooseCollege from "./components/ChooseCollege";
+import Admissions from "./components/Admissions";
+import FinancialAid from "./components/FinancialAid";
+import Housing from "./components/Housing";
+import Resources from "./components/Resources";
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
       <div className="App">
+        <Router>
           <NavBar />
-          <Home />
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/choose" element={<ChooseCollege/>} />
+              <Route path="/admissions" element={<Admissions/>} />
+              <Route path="/financialaid" element={<FinancialAid/>} />
+              <Route path="/housing" element={<Housing/>} />
+              <Route path="/resources" element={<Resources/>} />
+            </Routes>
+          </Router>
+
       </div>
-    </Router>
   );
 }
 
