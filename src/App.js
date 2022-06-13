f52ed870b8be46c4ac497a66194056ef2c23b41e
a-cole55 <arniqua_c@yahoo.com>
2022-06-13 15:25:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import HomeLogo from "./assets/college.png"
+import NavigationButton from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <img src="" alt="logo"></img>
+          <ul className="mainNav">
+            <li><a href="/choosing">Choosing a College</a></li>
+            <li><a href="/admissions">College Admissions</a></li>
+            <li><a href="/finaid">Paying for College</a></li>
+            <li><a href="/housing">Housing</a></li>
+            <li><a href="/resources">College Resources</a></li>
+          </ul>
+        </nav>
       </header>
+      <div className="home">
+        <div className="homeText">
+          <h1>College Guide</h1>
+          <span>Blah. Blah. Blah.</span>
+        </div>
+        <img src={HomeLogo} alt="homeLogo"></img>
+        <NavigationButton />
+      </div>
     </div>
   );
 }
