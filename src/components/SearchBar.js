75073b6bfe12849f995b10bd764bdb1a31f89dea
a-cole55 <arniqua_c@yahoo.com>
2022-06-16 21:16:24 -0500

    added college data for search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export default function SearchBar(){
+    return(
+        <form>
+        <label>Search College by Name:</label>
+        <input></input>
+        </form>
+    )
+}
