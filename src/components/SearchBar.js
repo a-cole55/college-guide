c4abda966973588ea6fc4e83efa8ffa2c27c2610
a-cole55 <arniqua_c@yahoo.com>
2022-06-19 13:32:59 -0500

    added functionality to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,69 @@
 import React, { useState } from 'react';
 import "./SearchBar.css";
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function SearchBar({placeholder, data}){
+
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleFilter = (event) => {
+        const wordSearched = event.target.value;
+        //clearing input functionality
+        setWordEntered(wordSearched)
+
+        //search through data and filter for correlating data
+        const newlyFiltered = data.filter((value) => {
+            return value.name.toLowerCase().includes(wordSearched.toLowerCase());
+        })
+
+        //open and close data box
+        if (wordSearched === "") {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newlyFiltered)
+        }
+    };
+    const clearInput = () => {
+        // empty data from array/list of search results
+        setFilteredData([]);
+        //remove text from input bar
+        setWordEntered("");
+    }
+
     return(
         <div  className='search'>
             <div className='searchInputs'>
-                <input type="text" placeholder={placeholder}/>
+                <input 
+                type="text" 
+                placeholder={placeholder}
+                value={wordEntered}
+                onChange={handleFilter} 
+                />
                 <div className='searchIcon'>
-                    <SearchIcon />
+                    {filteredData.length === 0 ? (
+                    <SearchIcon /> 
+                    ) : (
+                    <CloseIcon id="clearbtn" onClick={clearInput} /> 
+                )}
                 </div>
             </div>
-            <div className='dataResult'>
-                {data.map((value, key) => {
+            {filteredData.length !== 0 && (
+                <div className='dataResult'>
+                {filteredData.map((value, key) => {
                     return (
-                    <a className="dataItem" href={value.url} target="_blank">
+                    <a className="dataItem" href={value.url} target="_blank" rel="noopener noreferrer">
+                        {/* target blank allows you to open url in new tab */}
                         <p>{value.name}</p>
                     </a>
                     );
                 })}
             </div>
+            )
+
+            }
+            
         </div>
     )
 }
